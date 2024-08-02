14979e28bd5c420e2315af160df643ab5d2b08f1
Kristen Cullen <kristencullen@Kristens-Air-3.attlocal.net>
2024-08-01 22:26:26 -0500

    Update paths for public directory inside Develop folder

--- server.js
+++ server.js
@@ -8,15 +8,15 @@ const PORT = process.env.PORT || 3001;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(express.static('public'));
+app.use(express.static('Develop/public'));
 
 // HTML Routes
 app.get('/notes', (req, res) => {
-  res.sendFile(path.join(__dirname, 'public/notes.html'));
+  res.sendFile(path.join(__dirname, 'Develop/public/notes.html'));
 });
 
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'public/index.html'));
+  res.sendFile(path.join(__dirname, 'Develop/public/index.html'));
 });
 
 // API Routes
